2a733c01d5ff6186028f0a34184ebec67cb60158
Henzo-programas <148356014+Henzo-programas@users.noreply.github.com>
2025-11-17 20:33:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,72 @@
-/ criar a variavel com as cores dos botões..
-var corBotoes = ["vermelho","azul","verde","amarelo"];
-
+var corBotoes = ["vermelho", "azul", "verde", "amarelo"];
 var sequenciajogoComputador = [];
-
 var sequenciajogoUsuario = [];
-
 var inicio = false;
-
 var nivel = 0;
 
-$(".botao").click(function () {
-  var corEscolhidaUsuario = $(this).attr("id");
-  sequenciajogoUsuario.push(corEscolhidaUsuario);
-  playSound(corEscolhidaUsuario);
-  animatePress(corEscolhidaUsuario);
-  checkAnswer(sequenciajogoComputador.length - 1);  
-} )
+$(document).keypress(function () {
+  if (!inicio) {
+    $("#titulo").text("Nivel " + nivel);
+    nextSequence();
+    inicio = true;
+  } 
+});
+
+$(".botao").click(function (){
+    var corEscolhidaUsuario = $(this).attr("id");
+    sequenciajogoUsuario.push(corEscolhidaUsuario);
+    playSound(corEscolhidaUsuario);
+    checkAnswer(sequenciajogoUsuario.length - 1);
+});
+
+function checkAnswer(currentLevel) {
+    if (sequenciajogoComputador[currentLevel] === sequenciajogoUsuario[currentLevel]) {        
+        if (sequenciajogoUsuario.length === sequenciajogoComputador.length) {
+            setTimeout(function() {
+                nextSequence(); 
+            }, 2000); 
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("Game-Over");
+        $("#titulo").text("Fim do jogo! Pressione qualquer tecla para Reiniciar.");
+        setTimeout(function() {
+            $("body").removeClass("Game-Over");
+        }, 2000);
+        startOver();
+    }
+}
+
+function nextSequence() {
+    sequenciajogoUsuario = [];
+    nivel++;
+    $("#titulo").text("Nivel " + nivel);
+    var numeroAleatorio = Math.floor(Math.random() * 4); 
+    var corAleatoriaSorteada = corBotoes[numeroAleatorio]; 
+    sequenciajogoComputador.push(corAleatoriaSorteada);
+
+    $("#" + corAleatoriaSorteada)
+    .fadeIn(100) 
+    .fadeOut(100)
+    .fadeIn(100);
+
+    playSound(corAleatoriaSorteada);
+}
+
+function animaePress(currentColor){
+    $("#" + currentColor).addClass("Pressed");
+    setTimeout(function () {
+        $("#" + currentColor).removeClass("Pressed");
+    }, 100);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function startOver() {
+    nivel = 0;
+    sequenciajogoComputador = [];
+    inicio = false;
+}
