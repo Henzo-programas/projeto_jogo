14be30c7e5d284bcfad3dcfda7e07f05cb5851c6
Henzo-programas <148356014+Henzo-programas@users.noreply.github.com>
2025-11-17 19:16:42 -0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+/ criar a variavel com as cores dos botões..
+var corBotoes = ["vermelho","azul","verde","amarelo"];
 
+var sequenciajogoComputador = [];
+
+var sequenciajogoUsuario = [];
+
+var inicio = false;
+
+var nivel = 0;
+
+$(".botao").click(function () {
+  var corEscolhidaUsuario = $(this).attr("id");
+  sequenciajogoUsuario.push(corEscolhidaUsuario);
+  playSound(corEscolhidaUsuario);
+  animatePress(corEscolhidaUsuario);
+  checkAnswer(sequenciajogoComputador.length - 1);  
+} )
